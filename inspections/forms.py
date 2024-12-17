8fc2de91f2e346a3fd84b090e71aa73c9db49e9c
reza603 <rezababaei361@Gmail.com>
2024-12-17 16:37:05 +0330

    sourfacetoOnline2

--- inspections/forms.py
+++ inspections/forms.py
@@ -1,12 +1,10 @@
 from django import forms
 from .models import Inspection
-from jalali_date.fields import JalaliDateField
-from jalali_date.widgets import AdminJalaliDateWidget
 
 class InspectionForm(forms.ModelForm):
-  referdate = JalaliDateField(widget=AdminJalaliDateWidget)
+  referdate = forms.DateTimeField(widget=forms.DateInput(attrs={'type': 'date'}))
   done = forms.BooleanField(widget=forms.CheckboxInput(), required=False)
 
   class Meta:
-    model = Inspection
-    fields = ['task', 'user', 'company', 'referdate', 'done']
+   model = Inspection
+   fields = ['task', 'user', 'company', 'referdate']
